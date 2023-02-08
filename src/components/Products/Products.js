ffd5b8c68e55ed36e3635ddd4253608096975e53
shivam23kumar <shivamkumar.dev23@gmail.com>
2023-02-08 15:13:44 +0530

    React portal

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,25 +1,35 @@
 import {useEffect,useState} from "react";
 import ListItem from "../ListItems/ListItem";
 import axios from "axios";
+import Loader from "../UI/Loader";
 
 
 const Products = ()=>{
     
     const [item,setItem] = useState([])
+    const [loader,setLoader]=useState(true)
     useEffect(()=>{
         async function fetchedItems(){
-            const response = await axios.get('https://react-basics-37ea7-default-rtdb.firebaseio.com/items.json')
-            const data=response.data
-            const transformedData=data.map((item,index)=>{
-                return{...item,
-                id:index
+            try{
+                const response = await axios.get('https://react-basics-37ea7-default-rtdb.firebaseio.com/items.json')
+                const data=response.data
+                const transformedData=data.map((item,index)=>{
+                    return{...item,
+                    id:index
+                    }
+                })
+                setItem(transformedData)
+            }
+            finally{
+                setLoader(false)
+            }
                 }
-            })
-            setItem(transformedData)
-        }
+            
+            
         fetchedItems();
     },[])
     return (
+        <>
         <div className={"product-list"}>
             <div className={"product-list--wrapper"}>
                 {item.map((item)=>
@@ -29,6 +39,8 @@ const Products = ()=>{
                 <ListItem data={items[1]}></ListItem> */}
             </div>
         </div>
+        {loader && <Loader />}
+        </>
     )
 }
 
